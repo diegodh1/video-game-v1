5adf3d6fca1f890ede854dd3e03a8b39e8b8ac47
diegoalexanderdiazhernandez <diegodiazh1994@gmail.com>
2022-02-21 17:25:01 -0500

    añadiendo primer asset y agregando funcionalidad a las fisicas del juego

--- src/init.js
+++ src/init.js
@@ -7,17 +7,30 @@ const config ={
         preload: preload,
         create: create,
         update: update,
+    },
+    physics:{
+        default: 'arcade',
+        arcade:{
+            gravity:{
+                y: 500,
+
+            }
+        }
     }
 }
 
 var game = new Phaser.Game(config);
 
 function preload(){
-    console.log('hola soy preload');
+    this.load.image("bruno", "./assets/pixel-art-bruno.png")
 }
 
 function create(){
-    console.log('hola soy create');
+    this.bruno = this.physics.add.image(80, 100, "bruno");
+    this.bruno.setScale(0.1);
+    this.bruno.setCollideWorldBounds(true);
+    this.bruno.setBounce(0.5);
+    this.bruno.setVelocity(50,0);
 }
 
 function update(time, delta){
