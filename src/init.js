9fb6fde6f161bc916b0723563b14687522b7af32
diegoalexanderdiazhernandez <diegodiazh1994@gmail.com>
2022-02-21 19:40:15 -0500

    añadiendo funcionalidad de correr a bruno

--- src/init.js
+++ src/init.js
@@ -3,6 +3,7 @@ const config ={
     height: 360,
     parent:"container",
     type: Phaser.AUTO,
+    backgroundColor: '#469536',
     scene:{
         preload: preload,
         create: create,
@@ -22,17 +23,28 @@ const config ={
 var game = new Phaser.Game(config);
 
 function preload(){
-    this.load.image("bruno", "./assets/pixel-art-bruno.png")
+    this.load.spritesheet("bruno", "./assets/bruno-sprites.png", {frameWidth: 650, frameHeight:460});
 }
 
 function create(){
-    this.bruno = this.physics.add.image(80, 100, "bruno");
+    this.bruno = this.physics.add.sprite(80, 100, "bruno");
     this.bruno.setScale(0.1);
     this.bruno.setCollideWorldBounds(true);
     this.bruno.setBounce(0.5);
     this.bruno.setVelocity(50,0);
+    console.log(this.anims.generateFrameNumbers('bruno',{
+        frames:[0,1]
+    }))
+    this.anims.create({
+        key: 'bruno_walk',
+        frames: this.anims.generateFrameNumbers('bruno',{
+            frames:[0,1]
+        }),
+        frameRate: 12,
+        repeat: -1,
+    });
+    this.bruno.play('bruno_walk')
 }
 
 function update(time, delta){
-    console.log(delta);
 }
